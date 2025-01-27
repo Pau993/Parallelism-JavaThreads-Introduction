967af7c7fa18953bdb80341194c05c9b161507b9
nataliapaez993@gmail.com <nataliapaez993@gmail.com>
2025-01-27 00:21:39 -0500

    Carga los digitos de Pi, pero el millón se demora mucho, se realiza la prueba con 100 y 1000 y funciona

--- src/main/java/edu/eci/arsw/math/Main.java
+++ src/main/java/edu/eci/arsw/math/Main.java
@@ -7,6 +7,8 @@ package edu.eci.arsw.math;
 
 import java.util.Arrays;
 
+import edu.eci.arsw.threads.CountThread;
+
 /**
  *
  * @author hcadavid
@@ -17,6 +19,9 @@ public class Main {
         System.out.println(bytesToHex(PiDigits.getDigits(0, 10)));
         System.out.println(bytesToHex(PiDigits.getDigits(1, 100)));
         System.out.println(bytesToHex(PiDigits.getDigits(1, 1000000)));
+
+
+
     }
 
     private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
